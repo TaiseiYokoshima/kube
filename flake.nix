ca7038905fabb384437f6a603311cee493c2242f
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-09-08 15:11:19 +0100

    initial files

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+   description = "Development environment for cardamon";
+
+   inputs = {
+      nixpkgs.url = "nixpkgs/nixos-25.05";
+      flake-utils.url = "github:numtide/flake-utils";
+   };
+
+   outputs =
+      {
+         self,
+         nixpkgs,
+         flake-utils,
+      }:
+      flake-utils.lib.eachDefaultSystem (
+         system:
+         let
+            pkgs = import nixpkgs { inherit system; };
+         in
+         {
+            devShells.default = pkgs.mkShell {
+               buildInputs = with pkgs; [
+                  openssl
+                  pkg-config
+               ];
+
+               shellHook = ''
+                  exec ${pkgs.fish}/bin/fish
+               '';
+
+            };
+         }
+      );
+}
